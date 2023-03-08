6b72663903251ca3af6a17ca99e7e8df9b6cabd9
hnhine <hoainhi885@gmail.com>
2023-03-08 11:42:16 +0700

    add show function and command

--- lab1/1.student.mark.py
+++ lab1/1.student.mark.py
@@ -5,17 +5,23 @@ def input_Student_info():
     list_student = {"ID": [], "Fullname": [], "DoB": []}
 
     for i in range(int(total_stu)):
-        print("Information of student ",i," :")
+        print("Information of student ",i + 1," :")
         id = input("Enter student ID: ")
         name = input("Enter student full name: ")
         DoB = input("Enter date of birth: ")
         list_student["ID"].append(id)
         list_student["Fullname"].append(name)
         list_student["DoB"].append(DoB)
+        print("")
     return list_student
 
-list_students = input_Student_info()
-print(list_students)
+
+#function to show list students
+def show_students(list_students):
+    print("The list of students: ")
+    print(f" {'id'}             {'fullname'}              {'DoB'}  ")
+    for i in range(len(list_students['ID'])):
+        print(f"{list_students['ID'][i]}   {list_students['Fullname'][i]}{'      '}{list_students['DoB'][i]}")
 
 #function to enter course information
 def course_infor():
@@ -28,9 +34,14 @@ def course_infor():
         list_courses["ID"].append(id)
         list_courses["Course_Name"].append(name)
     return list_courses
-list_courses = course_infor()
-print(list_courses)
-#main_list = np.array(())
+
+#function to show course infor
+def show_course(list_course):
+    print("The list of courses: ")
+    print(f" {'id'}             {'course_name'} ")
+    for i in range(len(list_course['ID'])):
+        print(f"{list_course['ID'][i]}             {list_course['Course_Name'][i]}")
+
 
 #function to enter  mark for courses
 def input_mark(list_students, list_courses):
@@ -49,10 +60,10 @@ def input_mark(list_students, list_courses):
         else:
             print("That's course not exit!")
     return nested_course_mark
-mark_sheet = input_mark(list_students, list_courses)
 
-#function to show mark of a give course
-def show_mark(course_id, mark_sheet):
+
+#two function for showing mark
+def part_show_mark(course_id, mark_sheet):
     if course_id in mark_sheet["Course_id"]:
         index_course_id = 0
         for k in range(len(mark_sheet["Student_ID"])):
@@ -63,6 +74,64 @@ def show_mark(course_id, mark_sheet):
         for i in range(int(len(mark_sheet['Student_ID'])/len(mark_sheet['Course_id']))):
             #print(mark_sheet['Course_id'][index_course_id])
             print(mark_sheet['Student_ID'][index_course_id+i],": ",mark_sheet['Mark'][index_course_id+i])
+def show_mark(mark_sheet):
+    course_id = input("Enter the course id you want to show mark: ")
+    part_show_mark(course_id, mark_sheet)
 
-#listing function for mark of a given course
-show_mark('ict1', mark_sheet)
+def main_command():
+    print(
+        """
+        i1  : input student information
+        i2  : input course information
+        i3  : enter mark for a given course
+        s1  : show student information
+        s2  : show course information
+        s3  : show mark for a given course
+        e   : exit
+        """
+    )
+def main():
+
+    while True:
+        main_command()
+        cd = input("Choose your action: ").lower()
+
+        if cd == 'i1':
+            list_students = input_Student_info()
+            print("Now, you have the students list. Choose next action!")
+        elif cd == 'i2':
+            list_courses = course_infor()
+            print("Now, you have the course infor. Choose next action!")
+        elif cd == 'i3':
+            try:
+                mark_sheet = input_mark(list_students, list_courses)
+                print("Now, you have the marksheet. Choose next action!")
+            except:
+                print("Have you entered the list students or list courses yet?")
+        elif cd == 's1':
+            try:
+                show_students(list_students)
+            except:
+                print("Do not have students list!")  
+        elif cd == 's2':
+            try:
+                show_course(list_courses)
+            except:
+                print("Do not have course list!")
+
+        elif cd == 's3':
+            try:
+                show_mark(mark_sheet)
+            except:
+                print("Do not have the mark sheet!")
+        elif cd == 'e':
+            break
+        else:
+            print("That's command not exist")
+
+main()
+
+
+
+
+
